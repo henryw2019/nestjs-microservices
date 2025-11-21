48036108c1aad7e9843df58fdf8e4ab6ed96b3ea
henryw2019 <34269011+henryw2019@users.noreply.github.com>
2025-11-21 15:08:09 +0000

    webpack

--- apps/auth/webpack.config.js
+++ apps/auth/webpack.config.js
@@ -1,4 +1,4 @@
-export default (options, webpack) => {
+module.exports = (options, webpack) => {
   const lazyImports = [
     '@nestjs/microservices',
     '@nestjs/websockets',
@@ -9,7 +9,7 @@ export default (options, webpack) => {
     '@grpc/proto-loader',
     '@prisma/client',
   ];
-  
+
   return {
     ...options,
     target: 'node',
